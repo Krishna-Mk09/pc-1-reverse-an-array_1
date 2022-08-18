5b2912631040e562563603cc2cd9f5405aa9efc5
M V K <krishna.mugala9@gmail.com>
2022-08-18 22:31:58 +0530

    added source code and added java doc comments

--- src/main/java/com/jap/ReverseArrayDemo.java
+++ src/main/java/com/jap/ReverseArrayDemo.java
@@ -1,24 +1,34 @@
 package com.jap;
 
 public class ReverseArrayDemo {
-
     public static void main(String[] args) {
-
         //Declare and initialize values of an array.
-
+        int[] array = {23, 34, 43, 56, 67, 78, 43, 35};
         //Create an object of the class ReverseArrayDemo.
-
-       //Call the method getReverseArray and pass the array as parameter
-
-
+        ReverseArrayDemo reverseArrayDemo = new ReverseArrayDemo();
+        //Call the method getReverseArray and pass the array as parameter
+        int[] reverse = reverseArrayDemo.getReverseArray(array);
+        for (int i = 0; i < reverse.length; i++) {
+            System.out.println(reverse[i]);
+        }
     }
-
     //Write the logic to reverse the given array inside the below
     // method and return the reverse array.
-    public int[] getReverseArray(int [] array){
 
-        return new int[0];
-
-
+    /**
+     * It reverses the array.
+     *
+     * @param array the array to be reversed
+     * @return The reverse of the array.
+     */
+    public int[] getReverseArray(int[] array) {
+        int[] reverseArray = new int[array.length];
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                reverseArray[j] = array[i];
+            }
+        }
+        return reverseArray;
     }
 }
